cbd4aaa4eafe42e16c2f1dcdbeb30f6081e17374
Adlet <ksss90411@gmail.com>
2025-05-03 19:13:33 +0300

    added count of notification

---
+++ src/hooks/useGetCountNotification.ts
@@ -0,0 +1,19 @@
+import supabase from '@/utils/supabase'
+import { useQuery } from '@tanstack/react-query'
+
+function useGetCountNotification() {
+    return useQuery({
+        queryKey: ['countNotification'],
+        queryFn: async () => {
+            const { count, error } = await supabase
+                .from('notifications')
+                .select('*', { count: 'exact', head: true })
+
+            if (error) throw error
+
+            return count
+        },
+    })
+}
+
+export default useGetCountNotification
